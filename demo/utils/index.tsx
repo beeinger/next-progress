5a287026a485d0e128cc71ca35d3b8e69aa7cca5
beeinger <sulecki.maciek@gmail.com>
2021-10-17 16:34:54 +0100

    updating function delay to fit vercel max timeout

--- demo/utils/index.tsx
+++ demo/utils/index.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps } from "next";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // Something that takes a long time, simulating a heavy page
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 4500));
 
   return {
     props: {},
